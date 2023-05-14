42516f99d1fdad41a174930801b7aba041054988
insc15 <hieunm156@gmail.com>
2023-05-14 22:19:21 +0700

    update

--- main.js
+++ main.js
@@ -21,6 +21,17 @@ function getLectures(courseID) {
         return lectures;
     });
 }
+function getQuizzes(courseID) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.clear();
+        console.log('Getting quizzes...');
+        const res = yield fetch(`https://fpl.udemy.com/api-2.0/courses/${courseID}/subscriber-curriculum-items/?page_size=200&fields[lecture]=title,object_index,is_published,sort_order,created,asset,supplementary_assets,is_free&fields[quiz]=title,object_index,is_published,sort_order,type&fields[practice]=title,object_index,is_published,sort_order&fields[chapter]=title,object_index,is_published,sort_order&fields[asset]=title,filename,asset_type,status,time_estimation,is_external&caching_intent=True`);
+        const data = yield res.json();
+        const quizzes = data.results.filter((item) => item._class === 'quiz');
+        console.log('Got quizzes');
+        return quizzes;
+    });
+}
 function completeLecture(lectures) {
     return __awaiter(this, void 0, void 0, function* () {
         lectures.forEach((lecture) => __awaiter(this, void 0, void 0, function* () {
@@ -40,6 +51,63 @@ function completeLecture(lectures) {
         }));
     });
 }
+function completeQuiz(quizzes) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('Completing quizzes...');
+        for (const quiz of quizzes) {
+            console.log('Completing quiz: ' + quiz.title);
+            let userAttemptID = null;
+            try {
+                const previousAttempts_RES = yield fetch(`https://fpl.udemy.com/api-2.0/users/me/subscribed-courses/${courseID}/quizzes/${quiz.id}/user-attempted-quizzes/latest/?fields[user_attempted_quiz]=id,created,viewed_time,completion_time,version,completed_assessments,results_summary`);
+                const { id: previousAttemptID } = yield previousAttempts_RES.json();
+                if (!previousAttemptID || previousAttemptID.detail) {
+                    const userAttemptID_RES = yield fetch(`https://fpl.udemy.com/api-2.0/users/me/subscribed-courses/${courseID}/quizzes/${quiz.id}/user-attempted-quizzes/?fields[user_attempted_quiz]=id,created,viewed_time,completion_time,version,completed_assessments,results_summary`, {
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json;charset=UTF-8",
+                        },
+                        body: JSON.stringify({
+                            is_viewed: true,
+                        }),
+                    });
+                    const { id: userAttemptIDx } = yield userAttemptID_RES.json();
+                    userAttemptID = userAttemptIDx;
+                }
+                else {
+                    userAttemptID = previousAttemptID;
+                }
+            }
+            catch (error) {
+            }
+            const quizData_RES = yield fetch(`https://fpl.udemy.com/api-2.0/quizzes/${quiz.id}/assessments/?version=1&page_size=250&fields[assessment]=id,assessment_type,prompt,correct_response,section,question_plain,related_lectures`, {
+                method: "GET",
+                headers: {
+                    "content-type": "application/json;charset=UTF-8",
+                },
+            });
+            const { results: quizData } = yield quizData_RES.json();
+            for (const question of quizData) {
+                yield fetch(`https://fpl.udemy.com/api-2.0/users/me/subscribed-courses/${courseID}/user-attempted-quizzes/${userAttemptID}/assessment-answers/`, {
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json;charset=UTF-8",
+                    },
+                    body: JSON.stringify({
+                        assessment_id: question.id,
+                        duration: 15,
+                        response: JSON.stringify(question.correct_response),
+                    }),
+                });
+            }
+        }
+    });
+}
 getLectures(courseID).then((lectures) => {
-    completeLecture(lectures);
+    completeLecture(lectures).then(() => {
+        getQuizzes(courseID).then((quizzes) => {
+            completeQuiz(quizzes).then(() => {
+                console.log('Done');
+            });
+        });
+    });
 });
--- main.ts
+++ main.ts
@@ -10,6 +10,16 @@ async function getLectures(courseID: string | undefined | null) : Promise<[]> {
     return lectures;
 }
 
+async function getQuizzes(courseID: string | undefined | null) : Promise<[any]>  {
+    console.clear()
+    console.log('Getting quizzes...');
+    const res = await fetch(`https://fpl.udemy.com/api-2.0/courses/${courseID}/subscriber-curriculum-items/?page_size=200&fields[lecture]=title,object_index,is_published,sort_order,created,asset,supplementary_assets,is_free&fields[quiz]=title,object_index,is_published,sort_order,type&fields[practice]=title,object_index,is_published,sort_order&fields[chapter]=title,object_index,is_published,sort_order&fields[asset]=title,filename,asset_type,status,time_estimation,is_external&caching_intent=True`)
+    const data = await res.json();
+    const quizzes = data.results.filter((item: any) => item._class === 'quiz'); 
+    console.log('Got quizzes');
+    return quizzes;
+}
+
 async function completeLecture(lectures : []) : Promise<void>{
     lectures.forEach(async (lecture : any) => {
         const res = await fetch(`https://fpl.udemy.com/api-2.0/users/me/subscribed-courses/${courseID}/completed-lectures/`, {
@@ -28,6 +38,70 @@ async function completeLecture(lectures : []) : Promise<void>{
     });
 }
 
+async function completeQuiz(quizzes : [any]) : Promise<void>{
+    console.log('Completing quizzes...');
+    for (const quiz of quizzes) {
+        console.log('Completing quiz: ' + quiz.title);
+        let userAttemptID :any = null;
+        
+        try {
+            const previousAttempts_RES = await fetch(`https://fpl.udemy.com/api-2.0/users/me/subscribed-courses/${courseID}/quizzes/${quiz.id}/user-attempted-quizzes/latest/?fields[user_attempted_quiz]=id,created,viewed_time,completion_time,version,completed_assessments,results_summary`)
+            const { id : previousAttemptID } = await previousAttempts_RES.json();
+
+            if(!previousAttemptID || previousAttemptID.detail){
+                const userAttemptID_RES = await fetch(`https://fpl.udemy.com/api-2.0/users/me/subscribed-courses/${courseID}/quizzes/${quiz.id}/user-attempted-quizzes/?fields[user_attempted_quiz]=id,created,viewed_time,completion_time,version,completed_assessments,results_summary`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json;charset=UTF-8",
+                        },
+                        body: JSON.stringify({
+                            is_viewed: true,
+                        }),
+                    }
+                )
+                const { id : userAttemptIDx } = await userAttemptID_RES.json();
+                userAttemptID = userAttemptIDx;
+            }else{
+                userAttemptID = previousAttemptID;
+            }
+        } catch (error) {
+            
+        }
+        
+        const quizData_RES = await fetch(`https://fpl.udemy.com/api-2.0/quizzes/${quiz.id}/assessments/?version=1&page_size=250&fields[assessment]=id,assessment_type,prompt,correct_response,section,question_plain,related_lectures`,
+            {
+                method: "GET",
+                headers: {
+                    "content-type": "application/json;charset=UTF-8",
+                },
+            }
+        )
+        const { results: quizData } = await quizData_RES.json();
+        for (const question of quizData) {
+            await fetch(`https://fpl.udemy.com/api-2.0/users/me/subscribed-courses/${courseID}/user-attempted-quizzes/${userAttemptID}/assessment-answers/`,
+                {
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json;charset=UTF-8",
+                    },
+                    body: JSON.stringify({
+                        assessment_id: question.id,
+                        duration: 15,
+                        response: JSON.stringify(question.correct_response),
+                    }),
+                }
+            )
+        }
+    }
+}
+
 getLectures(courseID).then((lectures) => {
-    completeLecture(lectures);
+    completeLecture(lectures).then(() => {
+        getQuizzes(courseID).then((quizzes) => {
+            completeQuiz(quizzes).then(() => {
+                console.log('Done');
+            })
+        })
+    })
 })
